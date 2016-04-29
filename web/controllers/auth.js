e4bb3a4cb6961a3e657b57111fa8330f51fb043b
zenzhu <zic.zhu@gmail.com>
2016-04-29 12:46:51 +0800

    Clear comments

--- web/controllers/auth.js
+++ web/controllers/auth.js
@@ -1,23 +1,10 @@
-/* ================================================================
- * reliable-master by xdf(xudafeng[at]126.com)
- *
- * first created at : Tue Mar 17 2015 00:16:10 GMT+0800 (CST)
- *
- * ================================================================
- * Copyright xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
 
 const url = require('url');
 const qs = require('querystring');
 const bcrypt = require('bcryptjs');
-const GithubOauth = require('reliable-github');
-const GitlabOauth = require('reliable-gitlab');
+const GithubOauth = require('reliable-github-oauth');
+const GitlabOauth = require('reliable-gitlab-oauth');
 
 const user = require('./api/user');
 const _ = require('../../common/utils/helper');
