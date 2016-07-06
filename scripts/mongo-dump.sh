ee7dd67e380e9b4040a9591cb0ec687521417c57
xdf <xudafeng@126.com>
2016-07-06 13:39:37 +0800

    2.0 (#24)
    * tweak container
    
    * clean up script
    
    * tweak scripts
    
    * tweak dockerfile
    
    * update dump script

--- scripts/mongo-dump.sh
+++ scripts/mongo-dump.sh
@@ -2,6 +2,6 @@
 # Reference: http://dimafeng.com/2015/05/31/docker-mongo-backup/
 
 rm -rf /tmp/mongodump && mkdir /tmp/mongodump
-docker run -i --rm --link reliable_mongo_${RELIABLE_ENV_CONFIG}:mongo -v /tmp/mongodump:/tmp mongo bash -c 'mongodump -v --host $MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT --db '$1' --out=/tmp && chmod 777 /tmp/*'
+docker run -i --rm --link reliable_mongo:mongo -v /tmp/mongodump:/tmp mongo bash -c 'mongodump -v --host reliable_mongo:27017 --db reliable --out=/tmp && chmod 777 /tmp/*'
 tar -cvf $2 -C /tmp/mongodump $1
 rm -rf /tmp/mongodump
